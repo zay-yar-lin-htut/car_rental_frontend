d5c18a1aab21c35c04a7d54e2562acbeebc510da
zawwanahtue <yaza9036@gmail.com>
2025-07-26 09:43:43 +0630

    url change

--- src/services/BaseUrl.js
+++ src/services/BaseUrl.js
@@ -1,2 +1,2 @@
-// export const BaseUrl = "https://car-rental-web-xs37.onrender.com"
-export const BaseUrl = "http://localhost:8000";
+export const BaseUrl = "https://car-rental-web-xs37.onrender.com"
+// export const BaseUrl = "http://localhost:8000";
